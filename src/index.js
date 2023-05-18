e616eb8d96b9f76d96bd7577fe5a85b9e66688c0
jaqarrick <jcarrick@protonmail.com>
2023-05-18 01:01:41 -0400

    feat: video player and ios support

--- src/index.js
+++ src/index.js
@@ -1,83 +1,86 @@
-const dateTimeEl = document.querySelector("#date-time")
-const weatherDescriptionEl = document.querySelector("#weather-description")
-const footerDescriptionEl = document.querySelector("#footer-description")
-const videoStreamEl = document.querySelector("#video-stream")
-const logoEl = document.querySelector("#logo")
+document.addEventListener('DOMContentLoaded', () => {
+    const dateTimeEl = document.querySelector("#date-time")
+    const weatherDescriptionEl = document.querySelector("#weather-description")
+    const footerDescriptionEl = document.querySelector("#footer-description")
+    const videoStreamEl = document.querySelector("#video-stream")
+    const logoEl = document.querySelector("#logo")
 
-const sunBackgroundColor = "#E8F2AD"
-const rainBackgroundColor = "#A2CFEF"
-const sunTextColor = "#FF0000"
-const rainTextColor = "#0E5385"
-const city = "Brooklyn, NY"
+    const sunBackgroundColor = "#E8F2AD"
+    const rainBackgroundColor = "#A2CFEF"
+    const sunTextColor = "#FF0000"
+    const rainTextColor = "#0E5385"
+    const city = "Brooklyn, NY"
 
-const updateLogo = (isPrecipitation) => {
-    // ascii chars for the glyphs
-    const rain = "B"
-    const sun = "I"
-    logoEl.textContent = isPrecipitation ? rain : sun
-    logoEl.style.color = isPrecipitation ? rainTextColor : sunTextColor
-}
-
-const isMobile = () => {
-    const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
-    return viewportWidth < 800
-}
-const convertToSpans = text => [...text].map((char) => `<span>${char === " " ? "&nbsp;" : char}</span>`).join('')
-
-updateFooter = (isPrecipitation, description) => {
-    const alert = 'my roof might be leaking'
-    const descriptionSpans = convertToSpans(description)
-    const alertSpans = convertToSpans(alert)
-    const isMobileView = isMobile()
-    if (isMobileView) {
-        footerDescriptionEl.innerHTML = `<div class="footer-text-container">${description}</div>${isPrecipitation ? `<div class="footer-text-container">${alert}</div>` : ''}`
-    } else {
-        footerDescriptionEl.innerHTML = `<div class="footer-text-container">${descriptionSpans}</div>${isPrecipitation ? `<div class="footer-text-container">${alertSpans}</div>` : ''}`
+    const updateLogo = (isPrecipitation) => {
+        // ascii chars for the glyphs
+        const rain = "B"
+        const sun = "I"
+        logoEl.textContent = isPrecipitation ? rain : sun
+        logoEl.style.color = isPrecipitation ? rainTextColor : sunTextColor
     }
-    footerDescriptionEl.style.color = isPrecipitation ? rainTextColor : sunTextColor
-}
 
-const updateBackground = (isPrecipitation) => {
-    document.body.style.background = isPrecipitation ? rainBackgroundColor : sunBackgroundColor
-}
+    const isMobile = () => {
+        const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+        return viewportWidth < 800
+    }
+    const convertToSpans = text => [...text].map((char) => `<span>${char === " " ? "&nbsp;" : char}</span>`).join('')
 
-updateVideoDisplay = isPrecipitation => {
-    videoStreamEl.style.border = `solid 3px ${isPrecipitation ? rainTextColor : sunTextColor}`
-}
+    updateFooter = (isPrecipitation, description) => {
+        const alert = 'my roof might be leaking'
+        const descriptionSpans = convertToSpans(description)
+        const alertSpans = convertToSpans(alert)
+        const isMobileView = isMobile()
+        if (isMobileView) {
+            footerDescriptionEl.innerHTML = `<div class="footer-text-container">${description}</div>${isPrecipitation ? `<div class="footer-text-container">${alert}</div>` : ''}`
+        } else {
+            footerDescriptionEl.innerHTML = `<div class="footer-text-container">${descriptionSpans}</div>${isPrecipitation ? `<div class="footer-text-container">${alertSpans}</div>` : ''}`
+        }
+        footerDescriptionEl.style.color = isPrecipitation ? rainTextColor : sunTextColor
+    }
 
-const updateDescription = (isPrecipitation, dailyPercipitationSum) => {
-    weatherDescriptionEl.innerHTML = isPrecipitation ? `${dailyPercipitationSum} of rain in the last 24hrs in ${city}` : `It is <u>not</u> raining in ${city}`
-    weatherDescriptionEl.style.color = isPrecipitation ? rainTextColor : sunTextColor
-    dateTimeEl.style.color = isPrecipitation ? rainTextColor : sunTextColor
-}
-const updateTime = () => {
-    const currentTime = new Date();
-    const options = {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric',
-        timeZoneName: 'short'
-    };
-    const formattedTime = currentTime.toLocaleString('en-US', options);
-    dateTimeEl.textContent = formattedTime;
-}
+    const updateBackground = (isPrecipitation) => {
+        document.body.style.background = isPrecipitation ? rainBackgroundColor : sunBackgroundColor
+    }
+
+    updateVideoDisplay = isPrecipitation => {
+        videoStreamEl.style.border = `solid 3px ${isPrecipitation ? rainTextColor : sunTextColor}`
+    }
+
+    const updateDescription = (isPrecipitation, dailyPercipitationSum) => {
+        weatherDescriptionEl.innerHTML = isPrecipitation ? `${dailyPercipitationSum} of rain in the last 24hrs in ${city}` : `It is <u>not</u> raining in ${city}`
+        weatherDescriptionEl.style.color = isPrecipitation ? rainTextColor : sunTextColor
+        dateTimeEl.style.color = isPrecipitation ? rainTextColor : sunTextColor
+    }
+    const updateTime = () => {
+        const currentTime = new Date();
+        const options = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+            timeZoneName: 'short'
+        };
+        const formattedTime = currentTime.toLocaleString('en-US', options);
+        dateTimeEl.textContent = formattedTime;
+    }
 
 
-const init = async () => {
-    updateTime()
-    setInterval(updateTime, 1000);
-    const { isPrecipitation, description, dailyPercipitationSum } = await getWeatherData()
-    updateBackground(isPrecipitation)
-    updateLogo(isPrecipitation)
-    updateFooter(isPrecipitation, description)
-    updateDescription(isPrecipitation, dailyPercipitationSum)
-    updateVideoDisplay(isPrecipitation)
-    window.addEventListener('resize', () => {
+    const init = async () => {
+        updateTime()
+        setInterval(updateTime, 1000);
+        const { isPrecipitation, description, dailyPercipitationSum } = await getWeatherData()
+        updateBackground(isPrecipitation)
+        updateLogo(isPrecipitation)
         updateFooter(isPrecipitation, description)
-    })
-}
+        updateDescription(isPrecipitation, dailyPercipitationSum)
+        updateVideoDisplay(isPrecipitation)
+        window.addEventListener('resize', () => {
+            updateFooter(isPrecipitation, description)
+        })
+        initVideoPlayer(videoStreamEl, isPrecipitation)
+    }
 
-init()
+    init()
+})
---
+++ src/video-player.js
@@ -0,0 +1,60 @@
+const hlsStreamUrl = 'https://ext-tech.online/hls/mystream.m3u8'
+const dashStreamUrl = 'https://ext-tech.online/dash/mystream.mpd'
+const rainPosterSmall = 'assets/rain-poster-small.jpg'
+const rainPosterLarge = 'assets/rain-poster-large.jpg'
+const sunPosterSmall = 'assets/sun-poster-small.jpg'
+const sunPosterLarge = 'assets/sun-poster-large.jpg'
+
+const getBrowser = () => {
+    const bowserParser = bowser.getParser(window.navigator.userAgent).parsedResult;
+    const platformType = bowserParser.platform.type
+    return {
+        browser: bowserParser.browser.name,
+        isMobile: platformType === 'mobile' || platformType === 'tablet'
+    }
+}
+
+const showUnsupportedBrowserPoster = (videoEl, isMobile, isPrecipitation) => {
+    videoEl.controls = false
+    if (isMobile) {
+        videoEl.setAttribute("poster", isPrecipitation ? rainPosterSmall : sunPosterSmall)
+    } else {
+        videoEl.setAttribute("poster", isPrecipitation ? rainPosterLarge : sunPosterLarge)
+    }
+}
+
+const initVideoPlayer = (videoEl, isPrecipitation) => {
+    /* Because of inconsistencies in browser support for dash/hls we have to update the video
+    element for each browser and platform */
+    const { isMobile, browser } = getBrowser()
+    // for safari (desktop or ios we can use the native video element with the dash stream)
+    if (browser === 'Safari') {
+        videoEl.setAttribute("src", hlsStreamUrl)
+        videoEl.setAttribute("type", "application/x-mpegURL")
+        videoEl.addEventListener('loadedmetadata', function () {
+            // HLS stream metadata has been loaded
+            videoEl.setAttribute("controls", false)
+            videoEl.play();
+        });
+    } else if (isMobile) {
+        // afaik dash and hls aren't supported in other mobile browsers, so don't load stream
+        // show alert poster
+        showUnsupportedBrowserPoster(videoEl, isMobile, isPrecipitation)
+    } else if (Hls.isSupported()) {
+        const hls = new Hls();
+        // Load the HLS stream URL
+        hls.loadSource(hlsStreamUrl);
+        hls.attachMedia(videoEl);
+        // Optional: Handle HLS.js events
+        hls.on(Hls.Events.MANIFEST_PARSED, function () {
+            // HLS manifest has been parsed and stream is ready to play
+            video.play();
+        });
+    } else if (dashjs.supportsMediaSource()) {
+        const dashPlayer = dashjs.MediaPlayer().create();
+        dashPlayer.initialize(videoEl, dashStreamUrl);
+    } else {
+        showUnsupportedBrowserPoster(isMobile, isPrecipitation)
+    }
+}
+
